9429bf451c821fa8971764399e58e3061780d40f
Leo Osvald <leo.osvald@gmail.com>
2016-04-29 15:32:48 -0400

    Use scala-pickling to correctly serialize (cyclic) object graph

--- build.sbt
+++ build.sbt
@@ -10,10 +10,11 @@ libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)
 
 libraryDependencies ++= Seq(
   "com.softwaremill.scalamacrodebug" %% "macros" % "0.4",
-  "org.json4s" %% "json4s-native" % "3.3.0")
+  "org.json4s" %% "json4s-native" % "3.3.0" % "test",
+  "org.scala-lang.modules" %% "scala-pickling" % "0.10.1")
 
 libraryDependencies += "com.googlecode.kiama" %% "kiama" % "1.8.0"
 
-libraryDependencies += "com.lihaoyi" %% "utest" % "0.3.1" % "test"
+libraryDependencies += "com.lihaoyi" %% "utest" % "0.4.3" % "test"
 
 testFrameworks += new TestFramework("utest.runner.Framework")
--- src/test/scala/JsonClosureComparisonTest.scala
+++ src/test/scala/JsonClosureComparisonTest.scala
@@ -14,113 +14,118 @@
  * limitations under the License.
  */
 
-import org.json4s._
-import JsonDSL._
+import org.json4s._, JsonDSL._
 import native.{Serialization => JsonSer, _}
-import native.JsonMethods._
-// import org.json4s.jackson.{Serialization => JacksonSer} // FIXME: find dep.
+import native.JsonMethods.{pretty => jsonPretty, _}
+
+import org.kiama.output.PrettyPrinter._
+
+import scala.pickling.Defaults._, scala.pickling.json._
+import scala.reflect.runtime.universe._
 
 import utest._
 
-object JsonClosureComparisonTest extends TestBase { self =>
+object JsonClosureComparisonTest extends TestSuite { self =>
+  override def utestTruncateLength = 2000
+
+  def typeName[T : TypeTag] = typeOf[T].typeSymbol.name.toString
+
+  val noDiff = Diff(JNothing, JNothing, JNothing)
+
+  // Prettify JSON diffs (order-agnostic) in asserts using kiama library ;)
+  implicit def diff2PrettyStr(diff: Diff): String =
+    if (diff == noDiff) "" else pretty(any(diff))
+
   case class IntStr(i: Int, s: String)
   case class Nested(is: IntStr)
 
-  class StringInt(val s: String, val i: Int)
-
-  // TODO(hi-prio): use this from Jackson serialization (supported by json4s)
-  // @JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class,
-  //   property="@id")
   class IntAndSelfFieldClass {
     val x = 42
     val self = this
   }
 
-  val noDiff = Diff(JNothing, JNothing, JNothing)
-
-  val tests = TestSuite {
+  def tests = this {
     import SerializationUtil._
 
-    implicit def jValue2String(jv: JValue): String =
-      compact(render(jv))
-
     def toStr(jv: JValue) = compact(render(jv))
-
-    def jsonify(a: Any)(implicit fmts: Formats) =
-      toStr(Extraction.decompose(a)(fmts))
+    def toPrettyStr(jv: JValue) = jsonPretty(render(jv))
 
     "primitives" - {
-      implicit val fmts = JsonSer.formats(NoTypeHints)
       val one = 1
-      val oneTwo = jsonify(List(one, "two"))
-      assert(oneTwo == """[1,"two"]""")
 
       "singleton" - {
-        val rc1RefVals = fun1 { (s: String) => List(one, s) } freeRefVals
-        val actual = JsonSer.write(rc1RefVals)
-        assert(actual == "[1]")
+        val freeVals = fun1 { (s: String) => List(one, s) } freeRefVals
+
+        assert(freeVals.size == 1) // sanity check
+        val actual = "\\s+".r replaceAllIn(freeVals.head.pickle.value, "")
+        assert(actual == """{"$type":"scala.Int","value":1}""")
       }
 
       "int and string" - {
         // TODO(med-prio): use fun0 after it is properly implemented
         val two = "two"
         val rc1RefVals = fun1 { (a0: Unit) => List(one, two) } freeRefVals
-        val actual = JsonSer.writePretty(rc1RefVals)
-        assert(actual == """[
-          |  1,
-          |  "two"
-          |]""".stripMargin)
-        val d = parse(actual) diff parse("""[1, "two"]""")
-        assert(d == noDiff)
+        val serFreeVals = rc1RefVals map { _.pickle.value }
+        val diff = JArray(
+          ("$type" -> "scala.Int") ~ ("value" -> 1) ::
+            ("$type" -> "java.lang.String") ~ ("value" -> "two") :: Nil
+        ) diff JArray(serFreeVals map { parse(_) })
+        val diffStr: String = diff
+        assert(diffStr.isEmpty, diff == noDiff)
       }
     }
 
     "case class" - {
-      implicit val fmts = JsonSer.formats(
-        new FullTypeHints(classOf[IntStr] :: classOf[Nested] :: Nil))
-
-      val diff = Extraction.decompose(new IntStr(42, "foo")) diff (
-          ("i" -> 42) ~ ("s" -> "foo") ~
-            ("jsonClass" -> classOf[IntStr].getName))
-      assert(diff == noDiff)
+      val actual = new IntStr(42, "foo").pickle.value
+      val nameOfIntStr = // TODO(lo-prio): fragile, find a better way
+        IntStr.getClass.getName.dropRight(1).replaceAllLiterally("$", ".")
+      val diff = (
+        ("i" -> 42) ~ ("s" -> "foo") ~
+          ("$type" -> nameOfIntStr) // typeName[IntStr])
+      ) diff parse(actual)
+      assert(diff.isEmpty, diff == noDiff)
     }
 
     "regular class" - {
-      implicit val fmts = JsonSer.formats(
-        new FullTypeHints(classOf[StringInt] ::
-          classOf[IntAndSelfFieldClass] :: Nil))
-
       val (one, two) = ("one", 2)
 
-      "non circular" - {
-        val diff = Extraction.decompose(new StringInt(one, two)) diff (
-          ("jsonClass" -> classOf[StringInt].getName) ~
-            ("i", 2) ~
-            ("s", "one"))
-        assert(diff == noDiff)
+      "non circular local" - {
+        class StringInt(val s: String, val i: Int)
+        val diff = (
+          ("$type" -> "StringInt") ~ // FIXME(med-prio): avoid hard-coded name
+            ("i" -> two) ~
+            ("s" -> one) // order does not matter, it's a diff
+        ) diff parse(new StringInt(one, two).pickle.value)
+        assert(diff.isEmpty, diff == noDiff)
       }
 
       "circular ref" - {
         val circ = new IntAndSelfFieldClass
-        // {// FIXME(json4s): Classes defined in method bodies are not supported
-        //   val y = 11
-        // }
+        { // FIXME(scala-pickling): it serializes *nothing* without this,
+          val y = 11 // but this field is not even serialized! Double-bug?
+        }
         val freeVals = fun1 { (a0: Unit) => circ.x } freeRefVals
-        val expectedJson = (
-          ("jsonClass" -> classOf[IntAndSelfFieldClass].getName) ~
+        val expected = (
+          ("$type" -> circ.getClass.getName) ~
             ("self" -> (
-              ("jsonClass" -> classOf[IntAndSelfFieldClass].getName) ~
+              ("$type" -> circ.getClass.getName) ~
+                ("self" ->
+                  ("$ref" -> 0) // object identity info
+                ) ~
                 ("x" -> 42))) ~
             ("x" -> 42))
 
-        // FIXME: decompose doesn't pick up anything
-        // val diff = Extraction.decompose(freeVals head) diff (expectedJson)
-        // assert(diff == noDiff)
+        val actual = parse(freeVals.head.pickle.value)
+        val diff = expected diff actual
 
-        // FIXME: jsonify only picks up the class type but not object id!
-        val actual = jsonify(freeVals.head)
-        val expected = toStr(expectedJson)
-        // assert(actual == expected)
+        "no missing or extra" - {
+          assert(diff.added == JNothing, diff.deleted == JNothing)
+        }
+
+        "no changed" - {
+          val changed = diff.changed
+          assert(changed == JNothing)
+        }
       }
     }
   }
